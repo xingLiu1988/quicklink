4364e99c25b193d4bb5e990b3872b34808fd86bb
xingLiu1988 <77518946+xingLiu1988@users.noreply.github.com>
2021-03-24 21:32:22 -0400

    initial setup project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { JobDetailsComponent } from './components/job-details/job-details.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { EmployerComponent } from './components/employer/employer.component';
+import { EmployeeComponent } from './components/employee/employee.component';
+import { PostComponent } from './components/post/post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    JobDetailsComponent,
+    FooterComponent,
+    LoginComponent,
+    RegisterComponent,
+    EmployerComponent,
+    EmployeeComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
