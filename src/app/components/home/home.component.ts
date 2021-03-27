32f1b41d5688d9623ccae7d56a3d03d75619c778
xingLiu1988 <77518946+xingLiu1988@users.noreply.github.com>
2021-03-27 11:39:49 -0400

    change posts

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -10,14 +10,14 @@ export class HomeComponent implements OnInit {
 
   public posts: Post[] = [
     {postId: 1, companyName: 'company1', jobTitle: 'java developer', location: 'NY', salary: '500/day', phoneNumber: 911, description: 'come and join us1',postDate: 'DEC 30 2021', userId: 0, employerId: 1},
+    {postId: 2, companyName: 'company2', jobTitle: 'java developer2', location: 'NC', salary: '600/day', phoneNumber: 912, description: 'come and join us2',postDate: 'DEC 30 2021', userId: 0, employerId: 12},
     {postId: 2, companyName: 'company2', jobTitle: 'java developer2', location: 'FL', salary: '600/day', phoneNumber: 912, description: 'come and join us2',postDate: 'DEC 30 2021', userId: 0, employerId: 12},
     {postId: 2, companyName: 'company2', jobTitle: 'java developer2', location: 'FL', salary: '600/day', phoneNumber: 912, description: 'come and join us2',postDate: 'DEC 30 2021', userId: 0, employerId: 12},
     {postId: 2, companyName: 'company2', jobTitle: 'java developer2', location: 'FL', salary: '600/day', phoneNumber: 912, description: 'come and join us2',postDate: 'DEC 30 2021', userId: 0, employerId: 12},
     {postId: 2, companyName: 'company2', jobTitle: 'java developer2', location: 'FL', salary: '600/day', phoneNumber: 912, description: 'come and join us2',postDate: 'DEC 30 2021', userId: 0, employerId: 12},
     {postId: 2, companyName: 'company2', jobTitle: 'java developer2', location: 'FL', salary: '600/day', phoneNumber: 912, description: 'come and join us2',postDate: 'DEC 30 2021', userId: 0, employerId: 12},
     {postId: 2, companyName: 'company2', jobTitle: 'java developer2', location: 'FL', salary: '600/day', phoneNumber: 912, description: 'come and join us2',postDate: 'DEC 30 2021', userId: 0, employerId: 12},
-    {postId: 2, companyName: 'company2', jobTitle: 'java developer2', location: 'FL', salary: '600/day', phoneNumber: 912, description: 'come and join us2',postDate: 'DEC 30 2021', userId: 0, employerId: 12},
-    {postId: 2, companyName: 'company2', jobTitle: 'java developer2', location: 'FL', salary: '600/day', phoneNumber: 912, description: 'come and join us2',postDate: 'DEC 30 2021', userId: 0, employerId: 12},
+    {postId: 2, companyName: 'company2', jobTitle: 'java developer2', location: 'GA', salary: '600/day', phoneNumber: 912, description: 'come and join us2',postDate: 'DEC 30 2021', userId: 0, employerId: 12},
     {postId: 2, companyName: 'company2', jobTitle: 'java developer2', location: 'FL', salary: '600/day', phoneNumber: 912, description: 'come and join us2',postDate: 'DEC 30 2021', userId: 0, employerId: 12}
   ]
 
